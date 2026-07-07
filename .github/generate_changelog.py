293b78dbb5963007613553bb58b862843eb1a9df
alessandro <alessandro.casale.developer@gmail.com>
2026-07-07 21:13:13 +0200

    tech: Update tag range in changelog generator script

--- .github/generate_changelog.py
+++ .github/generate_changelog.py
@@ -12,7 +12,7 @@ def run(cmd):
 # -------------------------
 # 1. TAG RANGE
 # -------------------------
-tags = run(f"git tag --list '{addon}-v1.20.1'").split("\n")
+tags = run(f"git tag --list '{addon}-v*-1.20.1'").split("\n")
 tags = [t for t in tags if t]
 
 if tags:
